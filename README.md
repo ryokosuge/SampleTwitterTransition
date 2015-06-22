67b5468d8e26b043249dd27a08a7f0804dd9ee2c
programmatore <ryo.kosuge@gmail.com>
2015-06-22 11:30:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Twitter Modal Transition
---
+++ SampleTwitterTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleTwitterTransition
+//
+//  Created by nagisa-kosuge on 2015/06/22.
+//  Copyright (c) 2015年 RyoKosuge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
